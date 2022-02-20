0afd4dab396e7667a35fae71a1cf795a57c58397
Rigers Allaraj <74452104+rigers-allaraj@users.noreply.github.com>
2022-02-20 14:55:27 +0100

    Chat App whith next js

---
+++ pages/chats.js
@@ -0,0 +1,49 @@
+import React,{useState,useEffect,useContext} from "react";
+import { Context } from "../context";
+
+import { useRouter } from "next/dist/client/router";
+import dynamic from 'next/dynamic';
+import { route } from "next/dist/server/router";
+
+const ChatEngine = dynamic(() => 
+import("react-chat-engine").then((module) => module.ChatEngine)
+);
+
+const MessageFormSocial = dynamic(() => 
+import("react-chat-engine").then((module) => module.MessageFormSocial)
+);
+
+export default function Chats() {
+
+  const {username,secret} = useContext(Context);
+  const[showChat,setShowChat] =useState(false);
+  const router = useRouter();
+
+  useEffect(()=>{
+    if(typeof document !== null){
+      setShowChat(true)
+    }
+  })
+  
+
+  useEffect(() => {
+    if(username.length ===0 || secret.length ===0)
+     router.push('/')
+  })
+  //just return empty div if show caht false
+  if(!showChat) return <div />
+
+  return (
+    <div className="background">
+      <div className="shadow">
+      <ChatEngine
+         height='calc(100ch - 200px)'
+         projectID='981461af-105e-4db0-8f9f-f6b125134372'
+         userName={username}
+         userSecret={secret}
+         renderNewMessageForm={()=><MessageFormSocial/>}
+      />
+      </div>
+    </div>
+  )
+}
